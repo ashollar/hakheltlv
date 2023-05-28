7c4681de3e675aefbf56443b770c316a2254ea64
chabaddev <134392542+chabaddev@users.noreply.github.com>
2023-05-28 13:12:25 +0300

    Add files via upload

---
+++ standardhead.php
@@ -0,0 +1,25 @@
+    <link rel="icon" type="image/x-icon" href="favicon.png">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Corben:bold" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nobile" rel="stylesheet" type="text/css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Assistant&family=Bona+Nova&family=David+Libre&family=Frank+Ruhl+Libre&family=Heebo:wght@300&family=Noto+Rashi+Hebrew&family=Noto+Serif+Hebrew:wght@500&family=Open+Sans&family=Orbitron:wght@600&family=Rubik+Mono+One&family=Rubik+Moonrocks&family=Rubik:wght@300&family=Secular+One&display=swap" rel="stylesheet">
+<meta name="viewport" content="minimal-ui">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="keywords" content="mivtzoim,chabad,lubavitch,jewish,mitzva,hakhel,tel aviv" />
+<meta name="description" content="תכנס לעולם של הקהל" />
+<?php
+date_default_timezone_set("America/New_York");
+include "utils.php";
+#the speed of the css updating can be controlled from here
+echo "  <link href='theme.css?v=".getversion()."' rel='stylesheet'>";
+?>
