2b8fc12d9c3bb38c9064a19e079b7fa3a3ae1adb
chabaddev <134392542+chabaddev@users.noreply.github.com>
2023-05-28 22:59:46 +0300

    Update scrapehayom.php

--- scrapehayom.php
+++ scrapehayom.php
@@ -3,6 +3,6 @@
     $path="https://chabadlibrary.org/books/admur/hymym/1/1.htm";
     $xmlfile = file_get_contents($path);
     // Convert xml string into an object
-    $new = simplexml_load_string($xmlfile);
+    $new = simplexml_load_file($xmlfile);
     echo $new;
 ?>
