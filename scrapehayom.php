91bfd3abd65c10d57ade20f23b6a85d58dbc9c0f
chabaddev <134392542+chabaddev@users.noreply.github.com>
2023-05-28 23:32:40 +0300

    Update scrapehayom.php

--- scrapehayom.php
+++ scrapehayom.php
@@ -2,15 +2,21 @@
     $month='1';
     $day="1";
     $path="https://chabadlibrary.org/books/admur/hymym/".$month."/".$day.".htm";
-    $xmlfile = file_get_contents($path);
-    // Convert xml string into an object
-    $new = simplexml_load_string($xmlfile);
-    $con = json_encode($new);
-    // Convert into associative array
-    $newArr = json_decode($con, true);
-    $rawarray=$newArr['channel']['item'];
-    
-    print_r($rawarray);
+
+    // initialize the cURL request 
+$curl = curl_init(); 
+// set the URL to reach with a GET HTTP request 
+curl_setopt($curl, CURLOPT_URL, "https://scrapeme.live/shop/"); 
+// get the data returned by the cURL request as a string 
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); 
+// make the cURL request follow eventual redirects, 
+// and reach the final page of interest 
+curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true); 
+// execute the cURL request and 
+// get the HTML of the page as a string 
+$html = curl_exec($curl); 
+// release the cURL resources 
+curl_close($curl);
 
 
 ?>
