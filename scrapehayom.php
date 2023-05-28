0fb582eaeb23f46b6143cb538c238642dae8be28
chabaddev <134392542+chabaddev@users.noreply.github.com>
2023-05-28 23:09:27 +0300

    Update scrapehayom.php

--- scrapehayom.php
+++ scrapehayom.php
@@ -2,6 +2,7 @@
     $month='1';
     $day="1";
     $path="https://chabadlibrary.org/books/admur/hymym/".$month."/".$day.".htm";
+    echo $path;
 
     $dom = new DOMDocument();
     @$dom->loadHTMLFile($path);
