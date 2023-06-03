9d4a5392f27cfbf8c61b5c92e7879660b4f3ae43
chabaddev <134392542+chabaddev@users.noreply.github.com>
2023-06-03 23:13:20 +0300

    Update zmanim.php

--- zmanim.php
+++ zmanim.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
+
 <HTML>
   <head>
     <?php include "utils.php"?>
-
+    <meta name="viewport" content="minimal-ui">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
   </head>
  <body style="font-size:20px;text-align:center;margin:0px;">
- <div style='background-color:AntiqueWhite;width:100vw;height=100vh;margin:0px;>
+ <div style='background-color:AntiqueWhite;width:100vw;height=100vh;margin:0px;'>
 <p style='margin:0px;'>
 <?php
 
