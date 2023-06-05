6ccb1208888af161920ac8119ab3ba2c72920dcf
chabaddev <134392542+chabaddev@users.noreply.github.com>
2023-06-05 14:54:49 +0300

    Update zmanim.php

--- zmanim.php
+++ zmanim.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
   </head>
  <body style="font-size:40px;text-align:center;margin:0px;">
- <div style='background-image:url("background2.jpg.jpg");width:100vw;height=100vh;margin:0px;'>
+ <div style='background-image:url("background2.jpg.jpg");width:100%;height:100%;margin:0px;'>
 <p style='margin:0px;'>
 <?php
 
