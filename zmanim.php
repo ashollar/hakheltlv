4fae36c358b7272a375d9f39a813e0df36f1735c
chabaddev <134392542+chabaddev@users.noreply.github.com>
2023-06-05 15:00:33 +0300

    Update zmanim.php

--- zmanim.php
+++ zmanim.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="minimal-ui">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
   </head>
- <body style="font-size:40px;text-align:center;margin:0px;">
+ <body style="font-size:40px;text-align:center;margin:0px;height:100vh;">
  <div style='background-image:url("background2.jpg.jpg");width:100%;height:100%;margin:0px;'>
 <p style='margin:0px;'>
 <?php
