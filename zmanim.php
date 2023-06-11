e3ad2b3bfb98fc83c60774d61b3291a64cc9f733
chabaddev <134392542+chabaddev@users.noreply.github.com>
2023-06-11 16:32:41 +0300

    Update zmanim.php

--- zmanim.php
+++ zmanim.php
@@ -5,11 +5,14 @@
     <?php include "utils.php"?>
     <meta name="viewport" content="minimal-ui">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php $raw=zmanim();?>
   </head>
  <body style="font-size:80px;text-align:center;margin:0px;height:100vh;">
  <div style='background-image:url("background2.jpg.jpg");width:100%;height:100%;margin:0px;'>
 
- <H1 style="margin:0px;"><?php $raw=zmanim();echo $raw['סוף זמן קריאת שמע ']['time'];?></H1>
+
+ <p style="margin:0px;">סוף זמן קריאת שמע</p>
+ <H1 style="margin:0px;"><?php echo $raw['סוף זמן קריאת שמע ']['time'];?></H1>
 
 </div >
 </body>
