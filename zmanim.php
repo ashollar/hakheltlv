136cd875a9b92e48a8632997c3a173a74a8b0736
chabaddev <134392542+chabaddev@users.noreply.github.com>
2023-06-11 13:07:33 +0300

    Update zmanim.php

--- zmanim.php
+++ zmanim.php
@@ -8,26 +8,9 @@
   </head>
  <body style="font-size:80px;text-align:center;margin:0px;height:100vh;">
  <div style='background-image:url("background2.jpg.jpg");width:100%;height:100%;margin:0px;'>
-<p style='margin:0px;'>
-<?php
 
+ <H1><?php $raw=zmanim(); print_r($raw); ?></H1>
 
-
-$raw=zmanim();
-
-//print_r($raw);
-
-foreach ($raw as $item){
-    echo "<div style='display:grid;margin:0px;'>";
-    foreach ($item as $detail){
-        
-        echo "<div style='grid-row:1;'>".$detail."</div>";
-        
-    }
-    echo "</div>";
-}
-?>
-</p>
 </div >
 </body>
 </html>
