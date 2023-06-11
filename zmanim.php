4a091fb8d1a00adc5894ebbadabe02758cddccd9
chabaddev <134392542+chabaddev@users.noreply.github.com>
2023-06-11 16:37:11 +0300

    Update zmanim.php

--- zmanim.php
+++ zmanim.php
@@ -5,7 +5,7 @@
     <?php include "utils.php"?>
     <meta name="viewport" content="minimal-ui">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php $raw=zmanim(); $keys=array_keys($raw);foreach($keys as $key){echo '-'.$key.'-'}?>
+    <?php $raw=zmanim(); $keys=array_keys($raw);foreach($keys as $key){echo '-'.$key.'-';}?>
   </head>
  <body style="font-size:80px;text-align:center;margin:0px;height:100vh;">
  <div style='background-image:url("background2.jpg.jpg");width:100%;height:100%;margin:0px;'>
