2619f1ba96c6cac48a1a07696f8a82147220ee83
chabaddev <134392542+chabaddev@users.noreply.github.com>
2023-06-11 16:36:31 +0300

    Update zmanim.php

--- zmanim.php
+++ zmanim.php
@@ -5,13 +5,28 @@
     <?php include "utils.php"?>
     <meta name="viewport" content="minimal-ui">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php $raw=zmanim();?>
+    <?php $raw=zmanim(); $keys=array_keys($raw);foreach($keys as $key){echo '-'.$key.'-'}?>
   </head>
  <body style="font-size:80px;text-align:center;margin:0px;height:100vh;">
  <div style='background-image:url("background2.jpg.jpg");width:100%;height:100%;margin:0px;'>
 
 
- <p style="margin:0px;">סוף זמן קריאת שמע</p>
+ <H2 style="margin:0px;">סוף זמן קריאת שמע</H2>
+ <H1 style="margin:0px;"><?php echo $raw['סוף זמן קריאת שמע ']['time'];?></H1>
+
+ <H2 style="margin:0px;">סוף זמן קריאת שמע</H2>
+ <H1 style="margin:0px;"><?php echo $raw['סוף זמן קריאת שמע ']['time'];?></H1>
+
+ <H2 style="margin:0px;">סוף זמן קריאת שמע</H2>
+ <H1 style="margin:0px;"><?php echo $raw['סוף זמן קריאת שמע ']['time'];?></H1>
+
+ <H2 style="margin:0px;">סוף זמן קריאת שמע</H2>
+ <H1 style="margin:0px;"><?php echo $raw['סוף זמן קריאת שמע ']['time'];?></H1>
+
+ <H2 style="margin:0px;">סוף זמן קריאת שמע</H2>
+ <H1 style="margin:0px;"><?php echo $raw['סוף זמן קריאת שמע ']['time'];?></H1>
+
+ <H2 style="margin:0px;">סוף זמן קריאת שמע</H2>
  <H1 style="margin:0px;"><?php echo $raw['סוף זמן קריאת שמע ']['time'];?></H1>
 
 </div >
