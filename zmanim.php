fef0084f9f97264c0e737f2febfc559aa6b40ee1
chabaddev <134392542+chabaddev@users.noreply.github.com>
2023-06-18 15:06:26 +0300

    Update zmanim.php

--- zmanim.php
+++ zmanim.php
@@ -26,7 +26,8 @@ function checkTime(i) {
  <body onload="startTime()" style="font-size:80px;text-align:center;margin:0px;height:100vh;">
  
  <div style='background-image:url("background2.jpg.jpg");width:100%;height:100%;margin:0px;'>
- <div id="txt"></div>
+ 
+ <div id="txt" style='background-color:white;border-radius:10px;'></div>
 
  <H2 style="margin:0px;">סוף זמן קריאת שמע</H2>
  <H1 style="margin:0px;"><?php echo $raw['סוף זמן קריאת שמע ']['time'];?></H1>
